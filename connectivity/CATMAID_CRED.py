a0d803dde08e95227e43dcec123f0bde45da76c4
Timothy Warren <timlwarren@gmail.com>
2019-09-13 08:41:55 -0700

    added afternoon

--- connectivity/CATMAID_CRED.py
+++
@@ -1,8 +0,0 @@
-def CATMAID_CRED():
-    import pymaid
-    myInstance = pymaid.CatmaidInstance( 'https://neurocean.janelia.org/catmaidL1',
-                                         'Sales',
-                                         'snow tree branches',
-                                          '7fa63015863b26b647a00a9f420425bb2eef3a9b'
-                                         );
-    return (myInstance)
